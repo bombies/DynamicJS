013c8c6016be22b0cd5f5e32f1f48f4ff82fe0d5
bombies <xjamesdzn@gmail.com>
2021-10-14 01:19:42 -0500

    Move project to WebStorm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const Client = require('./structures/client.js');
 const client = new Client();
-const EmbedBuilder = require('./structures/embedbuilder.js');
 const config = require('./config.json');
 
 client.start(config.token);
